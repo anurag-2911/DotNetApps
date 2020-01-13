7c9035ed3101cc6871ccf51a3828eeac8a8b31b0
Anurag Kumar <anurag.kumar@microfocus.com>
2020-01-13 09:48:31 +0530

    x

---
+++ TestHost/Host.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using WCFServices;
+
+namespace TestHost
+{
+    class Host
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceHost = new ServiceHost(typeof(Service));
+            serviceHost.Description.Behaviors.Add(new CustomServiceBehavior());
+
+            serviceHost.Open();
+
+            Console.WriteLine("service is hosted successfully");
+            
+            Console.ReadKey();
+            
+        }
+
+        
+    }
+    
+}
